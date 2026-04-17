0eb0853698940e93b0660717ff6411be2b64a91b
Tomas <tomas@example.com>
2026-04-16 23:43:12 -0300

    fix: Se corrigio todo en tkinter_app

--- src/interfaces/ui/tkinter_app.py
+++ src/interfaces/ui/tkinter_app.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import threading
+from typing import List
+from src.core.models.position import Position
 
 
 class TkinterApp:
@@ -11,47 +13,63 @@ class TkinterApp:
         self.setup_ui()
 
     # ------------------------------------------
+    # Funciones asincronicas
+    # ------------------------------------------
+    def connect_terminal(self):
+        threading.Thread(target=self.connect_service, daemon=True).start()
+
+    def fetch_positions(self):
+        threading.Thread(target=self.render_positions, daemon=True).start()
+
+    ##Servicios
+    # Conectar con servicio
+    def connect_service(self):
+        try:
+            self.service.connect()
+            self.root.after(0, lambda: self.show_message("Connected to TWS"))
+        except Exception as e:
+            self.root.after(0, lambda err=e: self.show_error(str(err)))
+
+    def render_positions(self):
+        try:
+            positions = (
+                self.service.get_positions()
+            )  #! Positions deberia tener un tipo.
+            self.root.after(0, lambda: self.display_positions(positions))
+        except Exception as e:
+            self.root.after(0, lambda err=e: self.show_error(str(err)))
+
+    ##Render
+    # ---------------------------------------------
+    def display_positions(self, positions: List[Position]):
+        print("Positions")
+        for p in positions:
+            print(p)
+
+    ## Helpers functions
+    def show_message(self, msg):
+        print(msg)
+
+    def show_error(self, msg):
+        print("Error: ", msg)
+
+    # ------------------------------------------
     # UI setup
     # ------------------------------------------
     def setup_ui(self):
         # Boton conectar
         btn_connect = tk.Button(
-            self.root,
-            text="Connect",
-            command=lambda: threading.Thread(
-                target=self.service.connect, daemon=True
-            ).start(),
+            self.root, text="Connect", command=self.connect_terminal
         )
+
         btn_connect.pack()
 
         # Boton posiciones
         btn_positions = tk.Button(
             self.root, text="Get Positions", command=self.fetch_positions
         )
+
         btn_positions.pack()
-
-    # ------------------------------------------
-    # Thread Wrapper
-    # ------------------------------------------
-    def fetch_positions(self):
-        threading.Thread(target=self.render_positions, daemon=True).start()
-
-    # --------------------------------------------
-    # Logica
-    # --------------------------------------------
-    def render_positions(self):
-        positions = self.service.get_positions()
-
-        # Volver al hilo principal
-        self.root.after(0, lambda: self.display_positions(positions))
-
-    # ---------------------------------------------
-    # Render
-    # ---------------------------------------------
-    def display_positions(self, positions):
-        print("Positions")
-        for p in positions:
-            print(p)
 
     # ----------------------------------------------
     # Run the app
