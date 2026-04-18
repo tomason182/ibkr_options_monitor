547dedf301933877c89b563d9e663eec3bc048e4
Tomas <tomas@example.com>
2026-04-18 17:30:09 -0300

    add: basic table example

--- src/interfaces/ui/tkinter_app.py
+++ src/interfaces/ui/tkinter_app.py
@@ -9,24 +9,25 @@ class TkinterApp:
     def __init__(self, service):
         self.service = service
         self.root = tk.Tk()
-        self.style = ttk.Style()
+        #self.style = ttk.Style()
         self.root.title("Option Monitor App")
 
         # Variables globales
-        self.style.configure("Custom.Title", foreground=self.text_color, font=("Arial", 18, "bold"))
-        self.style.configure("subtile", foreground=self.text_color, font=("Arial", 14, "bold"))
-        self.style.configure("font_body", fg=self.text_color, font=("Arial", 12))
-        self.style.configure("font_body_bold", fg=self.text_color, font=("Arial", 12, "bold"))
+        #self.style.configure("Custom.Title", foreground=self.text_color, font=("Arial", 18, "bold"))
+        #self.style.configure("subtile", foreground=self.text_color, font=("Arial", 14, "bold"))
         
-        self.font_small = ("Arial", 10)
-        self.font_bottom = ("Arial", 11, "bold")
-        self.font_error = ("Arial", 12, "bold")
-        self.font_success = ("Arial", 12, "bold")
+        #self.style.configure("font_body", fg=self.text_color, font=("Arial", 12))
+        #self.style.configure("font_body_bold", fg=self.text_color, font=("Arial", 12, "bold"))
+        
+        #self.font_small = ("Arial", 10)
+        #self.font_bottom = ("Arial", 11, "bold")
+        #self.font_error = ("Arial", 12, "bold")
+        #self.font_success = ("Arial", 12, "bold")
 
-        self.color_error = "red"
-        self.text_color = "black"
-        self.success = "green"
-        self.color_mute = "gray"
+        #self.color_error = "red"
+        #self.text_color = "black"
+        #self.success = "green"
+        #self.color_mute = "gray"
 
         self.setup_ui()
 
@@ -36,15 +37,25 @@ class TkinterApp:
     def connect_terminal(self):
         threading.Thread(target=self.connect_service, daemon=True).start()
 
+    def disconnect_terminal(self):
+        threading.Thread(target=self.disconnect_service, daemon=True).start()
+
     def fetch_positions(self):
         threading.Thread(target=self.render_positions, daemon=True).start()
 
     ##Servicios
-    # Conectar con servicio
+    # Conectar | desconectar servicio
     def connect_service(self):
         try:
             self.service.connect()
             self.root.after(0, lambda: self.show_message("Connected to TWS"))
+        except Exception as e:
+            self.root.after(0, lambda err=e: self.show_error(str(err)))
+
+    def disconnect_service(self):
+        try:
+            self.service.disconnect()
+            self.root.after(0, lambda: self.show_message("Disconnected from TWS"))
         except Exception as e:
             self.root.after(0, lambda err=e: self.show_error(str(err)))
 
@@ -69,7 +80,7 @@ class TkinterApp:
         self.status_label.config(text=msg)
 
     def show_error(self, msg):
-        self.status_label.config(text=msg, fg="red")
+        self.status_label.config(text=msg)
         print("Error: ", msg)
 
     # ------------------------------------------
@@ -84,65 +95,89 @@ class TkinterApp:
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
         main_frame.columnconfigure(0, weight=1)
-        main_frame.rowconfigure(1, weight=1)
+        main_frame.rowconfigure(1, weight=0)
+        main_frame.rowconfigure(2, weight=1)
 
         title_label = ttk.Label(
-            main_frame, text="Monitor de opciones", style="Custom.Title"
+            main_frame, text="Monitor de opciones"
         )
-        title_label.grid(row=0, column=0, columnspan=2, pady=(0,20))
+        title_label.grid(row=0, column=0, columnspan=2, pady=(0,0))
 
         ## sections 
         self.setup_connection_section(main_frame,1 )
         self.setup_positions_section(main_frame, 2)
         
-        
+
+    
+
     def setup_connection_section(self, parent, row):
-        conn_section = ttk.LabelFrame(parent, text="Interactive Brokers Connection", padding="15")
-        conn_section.grid(row=row, column=0, sticky="we", pady=(0,15))
-        conn_section.columnconfigure(1,weight=1)
+        conn_frame = ttk.LabelFrame(parent, text="Interactive Brokers Connection", padding="15")
+        conn_frame.grid(row=row, column=0, sticky="nswe", pady=(15, 0))
+        conn_frame.columnconfigure(0,weight=1)
         #conn_section.columnconfigure(3, weight=1)  -- Para imputs
 
-        ttk.Label(conn_section, text="Host: 127.0.0.1, Port: 7496").grid(row=0, column=0, padx=(0,5), sticky="w")
+        ttk.Label(conn_frame, text="Host: 127.0.0.1 | Port: 7496").grid(row=0, column=0, padx=(0,5), sticky="we")
 
 
-        # Boton conectar
+        # Botones - conectar | desconectar
+        btn_frame = ttk.Frame(conn_frame)
+        btn_frame.grid(row=1, column=0, columnspan=4, pady=(10,0))
+
         btn_connect = ttk.Button(
-            parent,
+            btn_frame,
             text="Connect",
-            height=1,
-            width=12,
-            bg="#0f941e",
             command=self.connect_terminal,
         )
 
-        btn_connect.grid(row=0, column=0, sticky="w")
+        btn_connect.pack(side="left", padx=(0,10))
 
-        # Estado
+        btn_disconnect = ttk.Button(btn_frame, text="Disconnect", command=self.disconnect_terminal, state="disable")
+        btn_disconnect.pack(side="left")
 
-        status_title = tk.Label(
-            frame_connect, text="Status", font=self.font_subtitle, fg=self.text_color
+                # Refresh positions
+        btn_refresh = ttk.Button(
+            btn_frame, text="Refresh Positions", command=self.fetch_positions
         )
-        status_title.grid(sticky="ne")
 
-        self.status_label = tk.Label(
-            frame_connect,
-            text="Disconected",
-            fg="red",
-            font=self.font_small,
-            bg=self.color_mute,
-        )
-        self.status_label.grid(sticky="se")
+        btn_refresh.pack(side="left", padx=(10,0))
 
+
+        self.status_label = ttk.Label(
+            conn_frame, text="Disconnected", foreground="red")
+        self.status_label.grid(row=2, column=0, columnspan=4, pady=(5,0))
+
+    def setup_positions_section(self, parent, row):
         # Monitor
-        frame_monitor = tk.Frame(self.root, bd=2, relief="solid")
-        frame_monitor.grid(row=2, column=0)
+        monitor_frame = ttk.LabelFrame(parent, text="Options Monitor", padding="10")
+        monitor_frame.grid(row=row, column=0, sticky=("nwe"), pady=(15))
+        monitor_frame.columnconfigure(0, weight=1)
 
-        # Boton posiciones
-        btn_positions = tk.Button(
-            frame_monitor, text="Get Positions", command=self.fetch_positions
-        )
 
-        btn_positions.pack()
+        ## Tabla de posiciones
+        columns = ("id", "description","type", "strike", "position", "price", "total",  "last", "delta", "%_p_l_trade", "net_credit", "max_loss")
+        table = ttk.Treeview(monitor_frame, columns=columns, show="headings")
+
+        # titulos columnas
+        table.heading("id", text="id")
+        table.heading("description", text="Description")
+        table.heading("type", text="Type")
+        table.heading("strike", text="Strike")
+        table.heading("position", text="Position")
+        table.heading("price", text="Price")
+        table.heading("total", text="Total")                # total = price * 100 + fee
+        table.heading("last", text="Last")
+        table.heading("delta", text="Delta")
+        table.heading("%_p_l_trade", text="P/L x trade")
+        table.heading("net_credit", text="Net cretid")
+        table.heading("max_loss", text="Max loss")
+        # Insertar datos
+
+
+
+        table.pack()
+
+
+
 
     # ----------------------------------------------
     # Run the app
